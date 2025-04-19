7e1f9eadd23faa4f2aeadbbdb19c6535a09954af
epspafie <50031684+epspafie@users.noreply.github.com>
2025-04-19 20:11:41 +0900

    Create testSave.js

---
+++ testSave.js
@@ -0,0 +1,7 @@
+import { saveMessage } from './saveMessage.js';
+
+const testUserId = 'test-user-1';
+const testRole = 'user';
+const testMessage = '지은아, 오늘도 보고 싶어.';
+
+saveMessage(testUserId, testRole, testMessage);
